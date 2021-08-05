2fb09568d342801d2b80d31f64769a7dcae996cb
Valerie Wood <valerie.arsenault.pivotpoint@gmail.com>
2021-08-05 11:52:42 -0700

    Work completed for Task 4, creating visualizations and descriptions of data

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
 import pandas_profiling as pf
+import matplotlib.pyplot as plt
+
 path='../data/raw/adult.data'
 def load_and_process(path):
 
@@ -10,8 +11,8 @@ def load_and_process(path):
 
     df1 = (
         pd.read_csv('../data/raw/adult.data')
-        .drop([' State-gov', '39', ' 77516', ' 13', ' 2174', ' 0', ' 40', 'Unnamed: 0', ' Never-married', ' Adm-clerical', ' Not-in-family'], axis=1)
-        .rename({' State-gov': 'Work Class',' <=50K':'Income', ' White':'Race', ' Adm-clerical': 'Occupation', ' Bachelors': 'Education Level', ' Never-married': 'Marital-Status', ' Male': 'Sex', ' Not-in-family': 'Relationship', ' United-States': 'Native-Country'}, axis=1)
+        .drop([' 13', ' 2174', ' 0', ' 40', 'Unnamed: 0', ' Never-married', ' Not-in-family'], axis=1)
+        .rename({' State-gov': 'Work Class', ' 77516': 'Final Weight', ' <=50K':'Income', '39': 'Age', ' White':'Race', ' Adm-clerical': 'Occupation', ' Bachelors': 'Education Level', ' Never-married': 'Marital-Status', ' Male': 'Sex', ' Not-in-family': 'Relationship', ' United-States': 'Native-Country'}, axis=1)
     )
 
     # Method Chain 2 (Create new columns, drop others, and do processing)
@@ -27,3 +28,4 @@ def load_and_process(path):
     return df2 
 
 load_and_process(path)
+
